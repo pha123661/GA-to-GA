3075e48aea020db1e5a5cfef6c97cbacb262dccd
pha123661 <69206824+pha123661@users.noreply.github.com>
2021-05-07 01:12:58 +0800

    Update description

--- load_tsp.py
+++ load_tsp.py
@@ -5,7 +5,7 @@ import os
 def LoadTsp(path="./tsp_data"):
     '''
     Input: path to tsp datas
-    Output: list of 2D numpy array
+    Output: list of 2D TSP MAPs (Single 3D list)
     '''
     ret = list()
     for filename in os.listdir(path):
